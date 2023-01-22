1583fbdda75c7cda712eaa9b04cda4a374055dae
Magan Sharma <magansharma8122@gmail.com>
2023-01-22 13:55:46 +0530

    first sol

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,32 +1,29 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
+public class Boat implements WaterVehicle{
     String name;
     int capacity;
-
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
 
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
-        return name;
+        return this.name;
     }
 
     @Override
     public int getVehicleCapacity() {
-        return capacity;
+        return this.capacity;
     }
 }
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -6,37 +6,11 @@ public class Vehicle {
     private int currentSpeed;
     private int currentDirection;
 
-
-
     public Vehicle(String name) {
-        this.name=name;
-        this.currentDirection=0;
-        this.currentSpeed=0;
+        this.name = name;
+        this.currentSpeed = 0;
+        this.currentDirection = 0;
     }
-
-    public String getName() {
-        return name;
-    }
-
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
-//    public void setCurrentSpeed(int currentSpeed) {
-//        this.currentSpeed = currentSpeed;
-//    }
-
-    public int getCurrentDirection() {
-        return currentDirection;
-    }
-
-//    public void setCurrentDirection(int currentDirection) {
-//        this.currentDirection = currentDirection;
-//    }
 
     public void steer(int direction){
         //direction is in degrees, add it to the current direction
@@ -46,15 +20,25 @@ public class Vehicle {
 
     public void move(int speed, int direction){
         //set the values of currentSpeed and currentDirection
-        this.currentDirection=direction;
-        this.currentSpeed=speed;
+        this.currentSpeed = speed;
+        this.currentDirection = direction;
         System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
 
     public void stop(){
-        this.currentSpeed=0;
-
+        this.currentSpeed = 0;
         System.out.println("stop method called - The vehicle is stopped");
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
 }
